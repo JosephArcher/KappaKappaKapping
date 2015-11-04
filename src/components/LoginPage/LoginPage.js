a8ab701b6f8299faadbb2964ac36279575600ace
JosephArcher <joseph.archer1@marist.edu>
2015-11-04 18:01:53 -0500

    For real this time, fixing merge

---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,39 @@
+/*! React Starter Kit | MIT License | http://www.reactstarterkit.com/ */
+
+import React, { PropTypes, Component } from 'react';
+import styles from './LoginPage.css';
+import withStyles from '../../decorators/withStyles';
+const TextField = require('material-ui/lib/text-field');
+const RaisedButton = require('material-ui/lib/raised-button');
+
+@withStyles(styles)
+class LoginPage extends Component {
+
+  static contextTypes = {
+    onSetTitle: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const title = 'Login';
+    this.context.onSetTitle(title);
+    return (
+      <div className="LoginPage">
+        <div className="LoginPage-container">
+          <div className="LoginPage-components">
+            <h1 >{title}</h1>
+              <div >
+              <TextField floatingLabelText="Username" /></div>
+              <div >
+              <TextField floatingLabelText="Password" /></div>
+                <br></br>
+              <div >
+              <RaisedButton className="button" label="Submit" /></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default LoginPage;
