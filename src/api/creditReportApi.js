736d75e5442482002ff626fc8d61a023b3e72963
JosephArcher <joseph.archer1@marist.edu>
2015-12-04 12:47:19 -0500

    i can't spell names

--- src/api/creditReportApi.js
+++ src/api/creditReportApi.js
@@ -3,34 +3,21 @@
  */
 import pg from 'pg';
 import Router from 'express';
-
+ 
 let router = new Router();
 let conString = "postgres://postgres:wumbo100@localhost/kap";
-
+ 
 //var results = [];
-
+ 
 // // Grab data from the URL parameters
 // var id = req.params.todo_id;
-
+ 
 // // Grab data from http request
 // var data = {text: req.body.text, complete: req.body.complete};
-
-// let _userId = "testtest21";
-// let _password = "kappa";
-// //Will always be false!
-// let _is_admin = "false";
-// let _last_sign_in = "11-11-11";
-
-// let _school_id = 'DCC0000001';
-// let _first_name = 'Joe';
-// let _last_name = 'Blow';
-// let _email = 'kappa@gmail.kappa';
-// let _address = '10 Main Street';
-// let _phone_number = '1234567890';
-// let _birthday = '10-10-10';
-// let _intended_start_date = '10-10-10';
-
-let _selected_course_ids = ['00001','00002', '00003'];
+ 
+ 
+ 
+let _selected_course_ids = ['00001','00002', '00003', '00004', '00009'];
 let _total_credits = 0;
 let _credits_transfered = 0;
 let _transferable_course_ids = [];
@@ -40,54 +27,57 @@ let _transferable_courses = [];
 let _nontransferable_courses = [];
 let _programList = [];
 let _programListProgess = [];
-
+let _possible_program_requirements = [];
+let _possible_program_requirements_separated = [];
+let formatted_top_three = [];
+let topThree=[];
+let _pprs_lengths = [];
+ 
 // If the route matches /api/getTransferSchools...
 router.get('/', function(req, res) {
   // Set up the connection
   var results = [];
   var prettyResults = [];
-
+ 
   pg.connect(conString, function(err, client, done) {
-
+ 
     if (err) {
       done();
       console.log(err);
       return res.status(500).json({ success: false, data: err});
     }
-
-
+ 
+ 
     //TOTAL NUMBER OF CREDITS
-
+ 
     // Define the query
     var query = client.query('SELECT * FROM transfer_courses');
-
+ 
     // This query.on() call will run as many times as rows there are
     query.on('row', function(row, result) {
-      console.log("Transfer Courses:");
-      console.log(row.transfer_course_id);
-
+ 
+ 
       //for every transfer course, searches user's selected courses
       for (var i = 0; i < _selected_course_ids.length; i++) {
         //if found, adds its number of credits to total credits
         if(row.transfer_course_id== _selected_course_ids[i]){
           _total_credits= _total_credits + row.transfer_number_of_credits;
-          console.log(_total_credits);
+ 
         }
       };
-
+ 
       results.push(row);
     });
-
+ 
     //NUMBER OF CREDUTS TRANSFERED
-
+ 
     // Define the query
     var query = client.query('SELECT * FROM course_equivalents');
-
+ 
     // This query.on() call will run as many times as rows there are
     query.on('row', function(row, result) {
-      console.log("Courses Equ:");
-      console.log(row);
-
+ 
+ 
       //for every transfer course, searches user's selected courses
       for (var i = 0; i < _selected_course_ids.length; i++) {
         //if found, push course marist crn to transferable courses
@@ -96,58 +86,54 @@ router.get('/', function(req, res) {
           _credits_transfered = _credits_transfered + row.number_of_credits;
           _transferable_course_marist_crns.push(row.marist_crn);
           _transferable_course_ids.push(row.transfer_course_id);
-          console.log(_transferable_course_ids);
+ 
         }
-
+ 
       };
       results.push(row);
     });
-
+ 
     //TRANSFERABLE COURSES
-
+ 
     // Define the query
     var query = client.query('SELECT * FROM marist_courses');
-
+ 
     // This query.on() call will run as many times as rows there are
     query.on('row', function(row, result) {
-      console.log("Marist Courses:");
-      console.log(row);
-
+ 
       for (var i = 0; i < _transferable_course_marist_crns.length; i++) {
-
+ 
         if(row.marist_crn == _transferable_course_marist_crns[i]){
           _transferable_courses.push(row);
         }
       }
       results.push(row);
     });
-
+ 
 //NON TRANFERABLE COURSES
-
-
+ 
+ 
     // Define the query
     var query = client.query('SELECT * FROM transfer_courses');
-
-
+ 
+ 
     // This query.on() call will run as many times as rows there are
     query.on('row', function(row, result) {
-      //console.log("Transfer Courses:");
       //finds difference between transferable courses and selected courses
       var _nontransferable_course_ids = arr_diff(_transferable_course_ids, _selected_course_ids);
-
+ 
       //for every transfer course, searches user's selected courses
       for (var i = 0; i < _nontransferable_course_ids.length; i++) {
         //if found, adds its info to nontranferable courses
         if(row.transfer_course_id== _nontransferable_course_ids[i]){
           _nontransferable_courses.push(row);
-          //console.log(row.transfer_course_id);
         }
       };
-
+ 
       results.push(row);
     });
-
-
+ 
+ 
 //finds the difference between two arrays
     function arr_diff (a1, a2) {
       var a = [], diff = [];
@@ -166,81 +152,148 @@ router.get('/', function(req, res) {
       }
       return diff;
     };
-
-//Gets all Programs for now
-
-// Define the query
+ 
+//Selects all from programs 
+//and pushes to _programList
     var query = client.query('SELECT * FROM programs');
-
-
-// This query.on() call will run as many times as rows there are
+ 
     query.on('row', function(row, result) {
-      //console.log("Transfer Courses:");
-      //finds difference between transferable courses and selected courses
-      // var _nontransferable_course_ids = arr_diff(_transferable_course_ids, _selected_course_ids);
-
-      // //for every transfer course, searches user's selected courses
-      // for (var i = 0; i < _nontransferable_course_ids.length; i++) {
-      //    //if found, adds its info to nontranferable courses
-      //    if(row.transfer_course_id== _nontransferable_course_ids[i]){
-      //      _nontransferable_courses.push(row);
-      //console.log(row.transfer_course_id);
-      console.log("here");
-      console.log(row);
-      //}
-//};
-
+ 
       _programList.push(row);
     });
-
-
-//PERCENT COMPLETE
-// Define the query
+ 
+ 
+ 
+  //Selects all from program requirements
     var query = client.query('SELECT * FROM program_requirements');
-
-
-// This query.on() call will run as many times as rows there are
     query.on('row', function(row, result) {
-      //console.log("Transfer Courses:");
-      //finds difference between transferable courses and selected courses
-      // var _nontransferable_course_ids = arr_diff(_transferable_course_ids, _selected_course_ids);
-
-      // //for every transfer course, searches user's selected courses
-
-
-      for (var i = 0; i < _programList.length; i++) {
-        _programListProgess.push(_programList[i].program_name);
-        var tempArray = [];
-        if(_programList[i].program_id==row.program_id){
-          tempArray.push(row.program_id);
-
+ 
+      //compares transferable marist crn's to program requiremt crns
+      //pushes results to _possible_program_requirements
+      for (var i = 0; i < _transferable_course_marist_crns.length; i++) {
+       
+        if(_transferable_course_marist_crns[i]==row.marist_crn){
+          _possible_program_requirements.push(row);
+ 
         }
-        //    //if found, adds its info to nontranferable courses
-        //    if(row.transfer_course_id== _nontransferable_course_ids[i]){
-        //      _nontransferable_courses.push(row);
-        //console.log(row.transfer_course_id);
-        console.log("here");
-        console.log(_programListProgess);
-
-        _programListProgess.push(tempArray);
-      }
-
-      console.log(tempArray);
-      //}
-//};
-
-      // _programList.push(row);
+    };
+ 
     });
-
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+    //End query
     query.on('end', function() {
+ 
+ 
+    
+    //compares IDs from program list and _possible_program_requirements 
+    //to make an array of program requirements separted by programs
+    for (var j = 0; j < _programList.length; j++) {
+        //initializes temporary array for each loop
+        let tempArray = [];
+        for (var i = 0; i < _possible_program_requirements.length; i++) {
+             if(_programList[j].program_id==_possible_program_requirements[i].program_id){
+                 tempArray.push( _possible_program_requirements[i]);
+                 }
+             }
+             _possible_program_requirements_separated.push(tempArray);
+      }
+ 
+    
+ 
+    //creates array of amount fufilled of each program
+     for (var i = 0; i < _possible_program_requirements_separated.length; i++) {
+     _pprs_lengths.push(_possible_program_requirements_separated[i].length);
+        };
+ 
+      //sorts array of amount-program-fufilled lengths from least to most
+       _pprs_lengths.sort();
+ 
+       //sets minOfMaxes as the third most amount of class fufilling programs
+       let minOfMaxes = _pprs_lengths[ _pprs_lengths.length-3];
+ 
+    //loops through _possible_program_requirements_separated
+    //and returns array of top three programs fufilled
+    //if greater than min of maxes
+    //--to add top choices first
+    for (var i = 0; i < _possible_program_requirements_separated.length; i++) {
+       if(_possible_program_requirements_separated[i].length > minOfMaxes){
+       topThree.push(_possible_program_requirements_separated[i]);
+ 
+    //breaks loop after 3 are choosen
+      if(topThree.length==3){
+      i= i+42;
+            }
+       }
 
+      };
 
+  //loops through _possible_program_requirements_separated
+  //and returns array of top three programs fufilled
+  //if equal to min of maxes
+  //until length is three
+  
+  if(topThree.length<3){
+    
+    for (var i = 0; i < _possible_program_requirements_separated.length; i++) {
+      if(_possible_program_requirements_separated[i].length == minOfMaxes){
+      console.log(topThree);
+      topThree.push(_possible_program_requirements_separated[i]);
+      console.log(topThree);
+    //breaks loop after 3 are choosen
+      if(topThree.length==3){
+      i= i+42;
+            }
+       }
+ 
+      }
+  };
+    
+//formats top three programs to add details/percent complete
+ for(var i = 0; i < topThree.length; i++){
+   //initializes 
+    var program= topThree[i];
+    var programID= topThree[i][0].program_id;
+    var courseList =[];
+    var programName = "";
+    var program_num_class_required = "";
+ 
+    //loops through current programs' classes
+    for(var k = 0; k < program.length; k++){
+ 
+      //compares marist crn's from current program to transferable courses
+      //and pushes course details to courseList
+       for(var h = 0; h <  _transferable_courses.length; h++){
+         if(program[k].marist_crn==_transferable_courses[h].marist_crn){
+          courseList.push(_transferable_courses[h]);
+         }
+       }
+     
+    }
+ 
+ 
+    //loops through program list to set program name and # of classes required
+    for(var j = 0; j < _programList.length; j++){
+      if(programID==_programList[j].program_id){
+        programName = _programList[j].program_name;
+        program_num_class_required = _programList[j].classes_required;
+      }
+    }
+ 
+     //formats program name, course list and percent complete as object
+     var obj = new Object;
+     obj["Program_Name"] = programName;
+     obj["Completed_Courses"] = courseList;
+     obj["Percent_Complete"] = (Math.round(100*(courseList.length/program_num_class_required )));
+     
+    //pushes object to formatted array
+    formatted_top_three.push(obj);
+ 
+ };
+ 
+ 
       done();
       //pushes total number of credits to pretty results
       prettyResults.push({"Total_Credits" : _total_credits});
@@ -250,10 +303,9 @@ router.get('/', function(req, res) {
       prettyResults.push({"Transferable_Courses" : _transferable_courses});
       //pushes total number of credits to pretty results
       prettyResults.push({"Non_Transferable_Courses" : _nontransferable_courses });
-
-      prettyResults.push({"Top_Three_Programs" : _programList });
-      //console.log(res.json(results).transfer_course_id);
-
+      //pushes top three programs, classes transfered with each and percent complete
+      prettyResults.push({"Top_Three_Programs" : formatted_top_three });
+ 
       //reinitialize values after push
       _total_credits = 0;
       _credits_transfered = 0;
@@ -263,12 +315,19 @@ router.get('/', function(req, res) {
       _nontransferable_courses = [];
       _programList = [];
       _programListProgess = [];
-
+      _transferable_course_marist_crns = [];
+      _possible_program_requirements = [];
+      _possible_program_requirements_separated = [];
+      formatted_top_three = [];
+      topThree=[];
+      _pprs_lengths = [];
+ 
       return res.json(prettyResults);
     });
-
-
+ 
+ 
   });
 });
-
+ 
 export default router;
+ 
--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -3,12 +3,18 @@
 import React, { PropTypes, Component } from 'react';
 import withStyles from '../../decorators/withStyles';
 import styles from './RegisterPage.css';
-const TextField = require('material-ui/lib/text-field');
-const RaisedButton = require('material-ui/lib/raised-button');
-const DropDownMenu  = require('material-ui/lib/drop-down-menu');
+import {Form} from 'react-bootstrap';
+import {Input} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import {Grid} from 'react-bootstrap';
+import {Row} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import {MenuItem} from 'react-bootstrap';
+import DropdownButton from 'react-bootstrap';
 const injectTapEventPlugin = require("react-tap-event-plugin");
 import Link from '../Link';
 import Welcome from '../Welcome';
+const DoughnutChart = require("react-chartjs").Doughnut;
 
 
 @withStyles(styles)
@@ -23,87 +29,85 @@ class RegisterPage extends Component {
 
 
   render() {
-    injectTapEventPlugin();
 
-
-
-    const title = 'New User Registration';
+    const title = 'Student Registration';
     this.context.onSetTitle(title);
-    const intendedStartDates = [
-       { payload: '1', text: '' },
-       { payload: '2', text: 'Spring 2016' },
-       { payload: '3', text: 'Fall 2016' },
-       { payload: '4', text: 'Spring 2017' },
-       { payload: '5', text: 'Fall 2017' },
-       { payload: '6', text: 'Spring 2018' },
-    ];
-    const preferredMajors = [
-       { payload: '1', text: '' },
-       { payload: '2', text: 'Computer Science' },
-       { payload: '3', text: 'Math' },
-       { payload: '4', text: 'Physics' },
-       { payload: '5', text: 'Engineering' },
-    ];
-    const currentSchools = [
-       { payload: '1', text: '' },
-       { payload: '2', text: 'Dutchess Community College' },
 
-    ];
-
-   
-function handleNav(event) {
-console.log("yo");
-};
-
-
-   
-    return (
-      <div className="RegisterPage">
-        <div className="RegisterPage-container">
-          <h1>{title}</h1>
-       
-
-          <div className="RegisterPage-left">
-            <div><TextField hintText="First and Last Name" /></div>
-            <div><TextField hintText="Current School" /></div>
-            <div><TextField hintText="Email" /></div>
-            <div><TextField hintText="Address" /></div>
-            <div><TextField hintText="Phone Number" /></div>
-            <div><TextField hintText="Birthdate" /></div>
-            <br></br>
-
-          </div>
-          
-
-            <div className="RegisterPage-right">
-              <div><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    Intended Start Date</span> 
-                    <br></br>
-                    ><DropDownMenu menuItems={intendedStartDates} onChange={this.handleNav}/>
-              </div>
-              <div><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    Preferred Major</span> 
-                    <br></br
-                    ><DropDownMenu menuItems={preferredMajors} onChange={this.handleNav}/>
-              </div>
-              <div><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    Current School</span> 
-                    <br></br
-                    ><DropDownMenu menuItems={currentSchools} onChange={this.handleNav}/>
-              </div><br></br>
-                          <div>&nbsp;&nbsp;&nbsp;
-                          &nbsp;&nbsp;&nbsp;<a className="Navigation-link" href="/welcome" onClick={Link.handleClick}>
-                          <RaisedButton className="button" label="Submit" /></a></div>
-         
-
-            </div>
+  const percentageComplete = [
+    {
+        value: 75,
+        color: "#007fff",
+        highlight: "#00ffff"
+    },
+     {
+        value: 25,
+        color: "#FFFFFF",
+        highlight: "#FFFFFF"
+    }
+];
+    
 
   
+    return (
+     <div desktop={true} width={320}>
+      <div className="RegisterPage-container ">   
+      <Grid>
+        <div>
+        <form className="centeredLoginForm">
+        <h1>{title}</h1>
+        <Row>
+          <Col xs={6} md={6} lg={6}> 
+       
+                <Input type="text" bsSize="large" placeholder="First Name" />
+                <Input type="text" bsSize="large" placeholder="Last Name" />
+                <Input type="text" bsSize="large" placeholder="Email" />
+                <Input type="text" bsSize="large" placeholder="Phone Number" />
+                <Input type="text" bsSize="large" placeholder="Birthdate"/>
+          </Col>
+           <Col xs={6} md={6} lg={6}> 
 
+                    <Input bsSize="large" type="select">
+                     <option value="select">Intended Major</option>
+                     <option value="other">Not Sure Yet</option>
+                      <option value="other">Computer Science</option>
+                      <option value="other">Math</option>
+                      <option value="other">Fashion</option>
+                      <option value="other">Business</option>
+                      <option value="other">Communications</option>
+                  </Input>
 
-          </div>
+                <Input bsSize="large" type="select">
+                     <option value="select">Intended Start Date</option>
+                     <option value="other">Spring 2016</option>
+                      <option value="other">Fall 2016</option>
+                      <option value="other">Spring 2017</option>
+                      <option value="other">Fall 2017</option>
+                      <option value="other">Spring 2018</option>
+                      <option value="other">Fall 2018</option>
+                  </Input>
+
+                  <Input bsSize="large" type="select">
+                     <option value="select">Current School</option>
+                     <option value="other">Dutchess Community College</option>
+                  </Input>
+
+                <Row>
+                  <Col xs={12} sm ={12} md={4} lg={4}> 
+                    <DoughnutChart data={percentageComplete} width="75" height="75" />
+                  </Col>
+
+                  <Col xs={12} sm ={12} md={8} lg={8} > 
+                    <span className="percentageCompleteLabel">Percent Complete</span>
+                  </Col>
+              </Row>
+          </Col>
+      </Row>
+       <Button bsStyle="primary" block bsSize="large">Submit</Button>
+       </form>
+    </div>
+  </Grid>
         </div>
-      
+      </div>
     );
   }
 
